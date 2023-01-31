b7f296aefe69034e60bff5f3b47db0ad491ce695
Bulrock <maksim.volozhinskij@gmail.com>
2023-01-31 22:56:30 +0100

    feat: implement add userScore to the MongoDB

--- server.js
+++ server.js
@@ -13,12 +13,7 @@ const db = "mongodb+srv://Admin:nn6vJw.n7d3FXZs@cluster0.pzvo6cd.mongodb.net/gam
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  userHighScores: [
-    {
-      gamename: String,
-      score: String
-    },
-  ]
+  highScores: Object
 });
 
 const Users = mongoose.model('users', userSchema);
@@ -39,6 +34,7 @@ server.post("/user/login", function(request, response){
   console.log(request.body);
   const username = request.body.username;
   const password = request.body.password;
+
   Users.findOne({username: username}, (err, user) => {
     if(user?.password === password) {
       response.send({success: true, user})
@@ -52,6 +48,7 @@ server.post("/user/register", function(request, response){
   console.log(request.body);
   const username = request.body.username;
   const password = request.body.password;
+
   Users.exists({username: username}, (err, user) => {
     if(user === null) {
       Users.updateOne(
@@ -68,6 +65,50 @@ server.post("/user/register", function(request, response){
         });
     } else {
       response.send({success: false, error: "Username is already exists"})
+    }
+  })
+});
+
+server.post("/user/addscore", function(request, response){
+  console.log(request.body);
+  const username = request.body.username;
+  const gamename = request.body.gamename;
+  const score = request.body.score;
+
+  Users.findOne({username: username}, (err, user) => {
+    if(err) {console.log(`Error: ${err}`)}
+    console.log(user);
+    if(user !== null) {
+      if(user.highScores && user.highScores[gamename]) {
+        if(user.highScores[gamename] < score) {
+          const filter = {username: username};
+          user.highScores[gamename] = score;
+          const update = {highScores: user.highScores};
+
+          Users.findOneAndUpdate(filter, update, { upsert: true, new: true }, (err, user) => {
+            if(err) {
+              response.send({success: false, message: `Score wasn't updated: ${err.message}`});
+            } else {
+              response.send({success: true, message: "Score updated successfully"});
+            }
+          })
+        } else {
+          response.send({success: false, message: "Score is already up to date"});
+        }
+      } else {
+        const filter = {username: username};
+        user.highScores = user.highScores ? user.highScores : {};
+        user.highScores[gamename] = score;
+        const update = {highScores: user.highScores};
+
+        Users.findOneAndUpdate(filter, update, { upsert: true, new: true }, (err, user) => {
+          if(err) {
+            response.send({success: false, message: `Score wasn't updated: ${err.message}`});
+          } else {
+            response.send({success: true, message: "Score has been updated successfully"});
+          }
+        })
+      }
     }
   })
 });
